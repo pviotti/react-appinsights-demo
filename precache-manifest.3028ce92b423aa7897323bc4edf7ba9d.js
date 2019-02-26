4f53bac5ad90bdd37e8a0a9588921a06f35abd54
Paolo Viotti <pviotti@microsoft.com>
2019-02-26 14:34:47 +0000

    Updates

---
+++ precache-manifest.3028ce92b423aa7897323bc4edf7ba9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e37c53f880f5f271ad",
+    "url": "/react-appinsights-demo/static/css/main.b1928a7b.chunk.css"
+  },
+  {
+    "revision": "f5e37c53f880f5f271ad",
+    "url": "/react-appinsights-demo/static/js/main.f5e37c53.chunk.js"
+  },
+  {
+    "revision": "0f587878e6b5ba50ac3a",
+    "url": "/react-appinsights-demo/static/js/runtime~main.0f587878.js"
+  },
+  {
+    "revision": "72fb7e3c5cef2cf340de",
+    "url": "/react-appinsights-demo/static/js/2.72fb7e3c.chunk.js"
+  },
+  {
+    "revision": "1e2d221778c277f8431990697fe207e7",
+    "url": "/react-appinsights-demo/static/media/logo-ai.1e2d2217.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-appinsights-demo/static/media/logo-react.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e01da798892736b55361c761406da98",
+    "url": "/react-appinsights-demo/static/media/heart.4e01da79.svg"
+  },
+  {
+    "revision": "dd2c08da548de056bf94dd554ec1e712",
+    "url": "/react-appinsights-demo/index.html"
+  }
+];
