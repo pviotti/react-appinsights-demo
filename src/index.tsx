137901a573c699b9826292f1151292a5104117a8
Paolo Viotti <pviotti@microsoft.com>
2019-02-26 15:06:48 +0000

    fix public url basename

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-import { Router } from 'react-router-dom';
+import { Router  } from 'react-router-dom';
 import { ReactAI } from 'react-appinsights';
 import { createBrowserHistory } from "history"
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-const history = createBrowserHistory()
+const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 const IK = process.env.REACT_APP_APPINSIGHTS_KEY === undefined? "XXX" : process.env.REACT_APP_APPINSIGHTS_KEY;
 ReactAI.initialize({
     instrumentationKey: IK,
@@ -20,9 +19,9 @@ ReactAI.initialize({
     debug: true
 });
 
-ReactDOM.render(<Router history={history} basename={process.env.PUBLIC_URL} >
+ReactDOM.render(<Router  history={history} >
     <App />
-</Router>, document.getElementById('root'));
+</Router >, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
